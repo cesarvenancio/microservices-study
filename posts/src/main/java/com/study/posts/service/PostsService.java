29f8d7d64e36c111bce3d4420ea395dd964bde11
cvenancio <dolfo.venancio@gmail.com>
2019-04-29 15:52:29 +0100

    Simple code to short urls from posts text

--- posts/src/main/java/com/study/posts/service/PostsService.java
+++ posts/src/main/java/com/study/posts/service/PostsService.java
@@ -5,26 +5,28 @@ import java.util.List;
 
 import javax.transaction.Transactional;
 
+import org.apache.commons.validator.routines.UrlValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.study.posts.model.Posts;
 import com.study.posts.repository.PostsRepository;
 import com.study.posts.resource.PostsResource;
+import com.study.posts.resource.UrlResource;
 
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor
 @Service
 @Transactional
 public class PostsService {
 
 	@Autowired
 	private PostsRepository postsRepository;
+	
+	@Autowired
+    private TinyUrlClient tinyUrlClient;
 
 	public PostsResource createPost(PostsResource postsResource) {
 		
-	    Posts post = new Posts(null, postsResource.getPostText(), new Date());
+	    Posts post = new Posts(null, tinyUrlsFromText(postsResource.getPostText()), new Date());
 	    
 	    post = postsRepository.save(post);
 	    
@@ -42,4 +44,21 @@ public class PostsService {
 	public void deletePost(Long id) {
         postsRepository.deleteById(id);
     }
+	
+	private String tinyUrlsFromText(String text) {
+	    
+	    UrlValidator urlValidator = new UrlValidator();
+	    
+	    String [] words = text.split(" ");
+	    
+	    for (String word : words) {
+	        if(urlValidator.isValid(word)) {
+	            String shortUrl = tinyUrlClient.generateTinyUrl(new UrlResource(word)).getUrl();
+	            text = text.replace(word, shortUrl);
+	        }
+        }
+	    
+	    return text;
+	}
+	
 }
